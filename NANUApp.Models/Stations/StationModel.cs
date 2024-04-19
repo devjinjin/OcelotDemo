27a65ea407e84f2ac4aebce9a5723ed8721e2da9
devjinjin <devjinjin@gmail.com>
2024-04-19 19:43:39 +0900

    2개의 게이트웨이로 분리

---
+++ NANUApp.Models/Stations/StationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NANUApp.Models.Stations
+{
+    public class StationModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Lon { get; set; }
+        public decimal Lat { get; set; }
+
+
+
+    }
+}
---
+++ StationsService/Controllers/StationsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NANUApp.Models.Stations;
+
+namespace StationsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StationsController : ControllerBase
+    {
+        [Route("GetStationList")]
+        [HttpGet]
+        public async Task<List<StationModel>> GetStationList()
+        {
+            List<StationModel> Productlist = new List<StationModel>
+            {
+                new StationModel { Id=1,Name="강남세븐일레븐", Lon = 456, Lat=123 },
+                new StationModel { Id=1,Name="강북세븐일레븐", Lon = 456, Lat=123 },
+                new StationModel { Id=1,Name="강서세븐일레븐", Lon = 456, Lat=123 },
+                new StationModel { Id=1,Name="강동세븐일레븐", Lon = 456, Lat=123 },
+                new StationModel { Id=1,Name="성남세븐일레븐", Lon = 456, Lat=123 },
+
+            };
+
+            return Productlist;
+
+
+        }
+    }
+}
