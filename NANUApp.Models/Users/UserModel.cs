36425bee6c5f5479e12419b827a59c2152593e00
devjinjin <devjinjin@gmail.com>
2024-04-19 19:17:05 +0900

    초기 커밋.

---
+++ NANUApp.Models/Users/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NANUApp.Models.Users
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
---
+++ UsersService/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NANUApp.Models.Users;
+
+namespace UsersService.Controllers
+{
+
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [Route("GetUserDetails")]
+        [HttpGet]
+        public UserModel GetUserDetails()
+        {
+            UserModel obj = new UserModel();
+
+            obj.Email = "testemail@test.com";
+            obj.UserId = 1001;
+            obj.Name = "Test User";
+
+            return obj;
+        }
+
+    }
+}
